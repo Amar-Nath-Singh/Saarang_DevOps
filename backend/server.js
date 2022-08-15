d4402901c669cc6aa42a17103d80ca8ab3cfbe55
Amar-Nath-Singh <amarnathsingh.iitm@gmail.com>
2022-08-16 02:36:41 +0530

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI || "mongodb+srv://Saarang_Task:amarnath01@saarangtaskcluster.xxre5di.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true}
 );
 const connection = mongoose.connection;
